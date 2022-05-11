ba74ff7051ff9f4adc5d8818a7c261a1f2f875b6
GabrielMarcC <gabrielmarcelino099@gmail.com>
2022-05-10 21:16:12 -0300

    ate exercicio 7

--- modulo1/revisao-js/lista-revisao-js/exercicios.js
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -20,32 +20,73 @@ function retornaArrayInvertido(array) {
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-  
+    function ordernar( a, b){
+        return a - b
+    }
+    return array.sort(ordernar)
 }
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-  
+    let pares = []
+  for(let i = 0; i < array.length; i++){
+      if(array[i] % 2 == 0){
+          pares.push(array[i])
+      }
+  }
+  return pares
 }
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
- 
-}
+    let paresElevados2 = []
+    for(let i = 0; i <= array.length; i++){
+        if(array[i] % 2 == 0){
+            let numerosElevados = array[i] ** 2
+            paresElevados2.push(numerosElevados)
+        }
+    }
+    return paresElevados2
+}   
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  
+    retornaArrayOrdenado(array)
+    let maior = array[array.length -1]
+    return maior
 }
+
 
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
+    let propriedadesNumericas = {}
+    let maior = 0
+    let menor = 0
+    if(num1 > num2 && num2 < num1){
+        maior = num1
+    }else{
+        maior = num2
+    }if (num2 < num1 && num1 > num2){
+        menor = num2
+    }else{
+        menor = num1
+    }
+    
+    let diferenca = maior - menor
+    let divisivel = maior % menor == 0
+    
+    propriedadesNumericas = {
+        maiorNumero: maior,
+        maiorDivisivelPorMenor: divisivel,
+        diferenca: diferenca
 
+    }
+    return propriedadesNumericas
 }
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-   
+    
 }
 
 // EXERCÍCIO 09
