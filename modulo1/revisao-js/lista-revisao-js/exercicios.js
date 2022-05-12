c1cb34c80ed7ca335c88ea7b7802d895580048ac
GabrielMarcC <gabrielmarcelino099@gmail.com>
2022-05-12 17:00:09 -0300

    ate exercicio 13a

--- modulo1/revisao-js/lista-revisao-js/exercicios.js
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -86,37 +86,59 @@ function retornaObjetoEntreDoisNumeros(num1, num2) {
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-    
+    let numeros = []
+    for(let i = 0; i < n; i++){
+        numeros.push(i * 2)
+    }
+    return numeros
 }
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
-
+    if(ladoA === ladoB && ladoA == ladoC && ladoB === ladoC){
+        return 'Equilátero'
+    }else if(ladoA === ladoB || ladoA === ladoC  || ladoB === ladoC){
+        return 'Isósceles'
+    }else if (ladoA != ladoB && ladoA != ladoB != ladoC){
+        return 'Escaleno'
+    }
 }
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
-  
+  retornaArrayOrdenado(array)
+  let segundoMaior = array[array.length -2]
+  let segundoMenor = array[1]
+  let novoArray = [segundoMaior, segundoMenor]
+  return novoArray
 }
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-   
+    return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores[0]}, ${filme.atores[1]}, ${filme.atores[2]}, ${filme.atores[3]}.`
 }
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
-}
+   let novoObj = {...pessoa, nome: 'ANÔNIMO'
+   }
+   return novoObj
+} 
 
 // EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
-   
+   let pessoasPermitidas = pessoas.filter((altura) => {
+        return altura.altura >= 1.5
+   }).filter((idade) => {
+    return idade.idade > 14 && idade.idade < 60
+    })
+
+   return pessoasPermitidas
 }
 
 // EXERCÍCIO 13B
 function retornaPessoasNaoAutorizadas(pessoas) {
-  
+    
 }
 
 // EXERCÍCIO 14
