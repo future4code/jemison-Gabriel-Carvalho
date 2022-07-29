d0680b51e69376a361db3584acef0014ae582c47
GabrielMarcC <gabrielmarcelino099@gmail.com>
2022-07-28 22:22:07 -0300

    projeto labeusers

---
+++ modulo3/apis-labenusers/labenusers/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Cadastro from "./components/Cadastro"
+function App() {
+
+  return (
+    <div className="App">
+      <Cadastro/>
+    </div>
+  );
+}
+
+export default App;
---
+++ modulo3/apis-labenusers/labenusers/src/components/Styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const TelaUsuarios = styled.div `
+    display: flex;
+    flex-direction:column ;
+    justify-content: center;
+    align-items: flex-start;
+    margin: 10px;
+`
