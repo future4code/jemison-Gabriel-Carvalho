0911520fa5343b7b69932cada32873d6d63d6976
GabrielMarcC <gabrielmarcelino099@gmail.com>
2022-10-06 20:34:45 -0300

    exercicio 1 - revisão de js-backend

--- modulo5/revisao-javascript-backend/backend/exercicio1/index.js
+++ modulo5/revisao-javascript-backend/backend/exercicio1/index.js
@@ -1,1 +1,17 @@
-console.log("exercicio 4");
+const converteCelsius = (a, b) => {
+  if (
+    (isNaN(a) === true && b != "F") ||
+    (isNaN(a) === true && b != "K") ||
+    (a && b === "")
+  ) {
+    console.log(`Parâmetros inválidos`);
+  } else if (b === "F") {
+    const temperatura = (a * 9) / 5 + 32;
+    console.log(`${a}°C é ${temperatura}°F`);
+  } else if (b === "K") {
+    const temperatura = a + 273.15;
+    console.log(`${a}°C é ${temperatura}°K`);
+  }
+};
+
+converteCelsius(30, "K");
