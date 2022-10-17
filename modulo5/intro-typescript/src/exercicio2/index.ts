7893225719b2ebaeeaf54d52dd0355a989c328bb
GabrielMarcC <gabrielmarcelino099@gmail.com>
2022-10-17 10:57:31 -0300

    exercicio2 corrigido

--- modulo5/intro-typescript/src/exercicio2/index.ts
+++ modulo5/intro-typescript/src/exercicio2/index.ts
@@ -1,6 +1,9 @@
-function imprimeTresCoresFavoritas() {
-  const cor1: string = prompt("Insira sua primeira cor favorita");
-  const cor2: string = prompt("Insira sua segunda cor favorita");
-  const cor3: string = prompt("Insira sua terceira cor favorita");
-  console.log([cor1, cor2, cor3]);
+const cor1 = process.argv[2];
+const cor2 = process.argv[3];
+const cor3 = process.argv[4];
+
+function imprimeTresCoresFavoritas(cor1: string, cor2: string, cor3: string) {
+  return `Cores escolhidas: ${[cor1, cor2, cor3]}`;
 }
+
+console.log(imprimeTresCoresFavoritas(cor1, cor2, cor3));
